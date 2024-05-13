c79093e1812ceeeaae05fb0445d83242abe37eda
Levi Joyce <linuxgamer82@gmail.com>
2024-05-13 12:57:06 -0400

    Factory lesson one

--- FactoryPattern/Program.cs
+++ FactoryPattern/Program.cs
@@ -1,10 +1,18 @@
-﻿namespace FactoryPattern
+﻿using System.Threading.Channels;
+
+namespace FactoryPattern
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+         Console.WriteLine("How many wheels whould like on your vehicale, or type exit to quit");
+         var vehicaleType = Console.ReadLine();
+
+         var vehicaleName = VehicleFactory.GetRide(vehicaleType);
+
+            vehicaleName.Drive();
+            
         }
     }
 }
---
+++ FactoryPattern/VehicleFactory.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryPattern
+{
+   public static class VehicleFactory
+    {
+        public static IVehicle GetRide(string numberOfWheels)
+        {
+            while (true)
+            {
+                switch(numberOfWheels.ToLower())
+                {
+                    case "4":
+                        return new Car();
+
+                    case "2":
+                        return new Motorcycle();
+
+                    default:
+                        Console.WriteLine("Please choose 2 or 4 wheels");
+                        numberOfWheels = Console.ReadLine();
+                        break;
+                }
+
+            }
+        }
+    }
+}
